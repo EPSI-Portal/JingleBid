1017390522328d2c6c259f27bc2dedebdb60c680
stanislasbdx <s.castaybert@deal.fr>
2023-03-29 11:10:51 +0200

    ✨ feature・Ajout d'une méthode globale pour appeler l'API

--- src/components/toLoad/functions.js
+++ src/components/toLoad/functions.js
@@ -1,18 +1,20 @@
+import axios_ from "axios";
+
 export let functions = {
 	goTo: function (link, param = "_blank") {
 		window.open(link, param);
 	},
 
 	findWithAttr: function (array, attr, value) {
-		for(var i = 0; i < array.length; i += 1) {
-			if(array[i][attr] === value) {
+		for (var i = 0; i < array.length; i += 1) {
+			if (array[i][attr] === value) {
 				return i;
 			}
 		}
 		return -1;
 	},
 
-	getNextIndex: function(array, n) {
+	getNextIndex: function (array, n) {
 		var numbers = [];
 
 		if (array.length > 1) {
@@ -27,6 +29,15 @@ export let functions = {
 			return Math.max(...numbers) + 1;
 		} else {
 			return 1;
+		}
+	},
+
+	callAPI: async function (url) {
+		try {
+			const response = await axios_.get(url);
+			return response.data;
+		} catch (error) {
+			console.error(error);
 		}
 	},
 };
