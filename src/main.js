9089f68381807a63e51adc6037b0424e0c587186
stanislasbdx <s.castaybert@deal.fr>
2023-03-29 11:39:34 +0200

    🚀 workspace upgrade・Ajout de Axios, Moment et UUID

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import router from './router'
 
 import './assets/main.css'
 
+import axios from 'axios'
+Vue.prototype.$_axios = axios
+
+import moment_ from 'moment'
+Vue.prototype.$_moment = moment_
+moment_.locale('fr')
+
 const app = createApp(App)
 
 app.use(router)
