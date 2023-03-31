28670655a0b8ae8f80380006ab3e52c6ecae51a7
stanislasbdx <s.castaybert@deal.fr>
2023-03-31 22:07:02 +0200

    🌈 style・Affichage produit

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -3,18 +3,13 @@
 		<HeaderComponent :title="data.name"></HeaderComponent>
 		<v-row>
 			<v-col cols="8">
-				<v-card class="glasscard">
-
-					<v-carousel hide-delimiter-background delimiter-icon="fas fa-minus" next-icon="fas fa-caret-right" prev-icon="fas fa-caret-left">
-						<v-carousel-item v-for="img in images" :key="img" :src="img">
-						</v-carousel-item>
-					</v-carousel>
-
+				<v-card>
+					<v-img :src="data.image" cover height="45vh"></v-img>
 				</v-card>
 
-				<v-card class="glasscard my-3">
+				<v-card class="my-3">
 					<v-card-text>
-						<h3>Description</h3>
+						<h3 class="text-h4 mb-2">Description</h3>
 						<p>
 							{{ data.shortDesc }}
 						</p>
@@ -23,35 +18,38 @@
 						</p>
 					</v-card-text>
 				</v-card>
-
 			</v-col>
 			<v-col cols="4">
-				<v-card class="glasscard">
-					<v-card-title>
+				<v-card class="pb-3">
+					<v-card-title class="text-h5 font-weight-regular mb-4 mt-1">
 						Informations en direct
 					</v-card-title>
 					<v-card-text>
 
-						<v-row class="overline" style="font-size: 1.2em!important;">
-							<v-col cols="8" class="py-0">
-								<span class="text-caption" style="font-size: 1em!important;">Prix actuel</span>
+						<v-row>
+							<v-col class="py-0">
+								<span>Prix actuel</span>
 							</v-col>
-							<v-col cols="4" align="right" class="py-0">
-								{{ Number(data.prices.actual).toLocaleString(undefined, {minimumFractionDigits: 2}) }}€
+							<v-col cols="auto" class="py-0">
+								<span class="text-overline">{{ Number(data.price).toLocaleString(undefined, {minimumFractionDigits: 2}) }}€</span>
 							</v-col>
+						</v-row>
 
-							<v-col cols="8" class="py-0">
-								<span class="text-caption" style="font-size: 1em!important;">Prix de départ</span>
+						<v-row>
+							<v-col class="py-0">
+								<span>Prix de départ</span>
 							</v-col>
-							<v-col cols="4" align="right" class="py-0">
-								{{ Number(data.prices.start).toLocaleString(undefined, {minimumFractionDigits: 2}) }}€
+							<v-col cols="auto" class="py-0">
+								<span class="text-overline">{{ Number(data.price).toLocaleString(undefined, {minimumFractionDigits: 2}) }}€</span>
 							</v-col>
+						</v-row>
 
-							<v-col cols="8" class="py-0">
-								<span class="text-caption" style="font-size: 1em!important;">Temps restant</span>
+						<v-row>
+							<v-col class="py-0">
+								<span>Temps restant</span>
 							</v-col>
-							<v-col cols="4" align="right" class="py-0">
-								{{ $_moment().to(data.timeOptions.bidStopTime, true) }}
+							<v-col cols="auto" class="py-0">
+								<span class="text-overline">{{ $_moment().to(data.timeOptions.bidStopTime, true) }}</span>
 							</v-col>
 						</v-row>
 
@@ -62,14 +60,13 @@
 					</v-card-title>
 					<v-list style="background-color: rgba( 255, 255, 255, 0 )" class="pt-0">
 						<v-list-item-group disabled>
-							<v-list-item v-for="(item, i) in 5" :key="i">
-								<v-list-item-icon>
-									<v-icon color="secondary">far fa-money-bill-alt</v-icon>
-								</v-list-item-icon>
-
+							<v-list-item v-for="(item, i) in 3" :key="i">
 								<v-list-item-content>
-									<v-list-item-title>{{ randomUsers[Math.floor(Math.random() * randomUsers.length)] }}</v-list-item-title>
-									<v-list-item-subtitle>{{ Math.round(Number(data.prices.actual) + (Math.random() * 5)).toLocaleString(undefined, {minimumFractionDigits: 2}) }}€</v-list-item-subtitle>
+									<v-list-item-title>
+										<v-icon color="secondary" class="mr-2" size="small">far fa-money-bill-alt</v-icon>
+										<span class="mt-2">{{ randomUsers[Math.floor(Math.random() * randomUsers.length)] }}</span>
+									</v-list-item-title>
+									<v-list-item-subtitle>{{ Math.round(Number(data.price) + (Math.random() * 5)).toLocaleString(undefined, {minimumFractionDigits: 2}) }}€</v-list-item-subtitle>
 								</v-list-item-content>
 							</v-list-item>
 						</v-list-item-group>
@@ -82,7 +79,7 @@
 					</v-card-title>
 					<v-row>
 						<v-col cols="12" sm="6">
-							<v-text-field variant="underlined" :label="`Proposez un prix (> ${data.prices.actual}€)`" :min="Number(data.prices.actual)" suffix="€" type="number" auto-grow rows="1" class="mx-3 py-0"></v-text-field>
+							<v-text-field variant="underlined" :label="`Proposez un prix (> ${data.price}€)`" :min="Number(data.price)" suffix="€" type="number" auto-grow rows="1" class="mx-3 py-0"></v-text-field>
 						</v-col>
 						<v-col cols="12" sm="6" class="pt-1">
 							<v-checkbox v-model="anonymCheckbox" label="Je préfère être anonyme" class="ml-4 py-0"></v-checkbox>
@@ -97,8 +94,8 @@
 
 				</v-card>
 
-				<v-card class="glasscard mt-6">
-					<v-card-title>
+				<v-card class="mt-6">
+					<v-card-title class="text-h6 font-weight-regular mt-1">
 						Achetez en tout sécurité
 					</v-card-title>
 
@@ -117,7 +114,7 @@
 
 	export default {
 		name: "ProductPage",
-		inject: ["$models", "$_moment"],
+		inject: ["$models", "$_moment", "$_axios"],
 
 		props: {
 			id: {
@@ -138,13 +135,22 @@
 			}
 		},
 
-	created() {
+	async created() {
 			//TODO: Get product
 			/*this.$db.collection("products").doc(this.id).get().then((res) => {
 				this.data = res.data();
 
 				this.images = [this.data.img.main, this.data.img.secondary];
 			});*/
+			try {
+				const response = await this.$_axios.get(import.meta.env.VITE_API_URL + "/product/" + this.id);
+				this.data = {
+					...response.data,
+					timeOptions: {}
+				};
+			} catch (error) {
+				console.error(error);
+			}
 		}
 	}
 
